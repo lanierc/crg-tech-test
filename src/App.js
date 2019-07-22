7804c1746a3e206fb3a197bd9b69477f30f2d679
Corey <iam@coreylanier.com>
2019-07-22 15:29:35 -0400

    beginning header/searchbar creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import logo from "./logo.svg";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends React.Component {
@@ -9,20 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
       </div>
     );
   }
