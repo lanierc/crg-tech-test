bd7c4e6b36f0f4ee7f49ae95fdbd0198837d206c
Corey <iam@coreylanier.com>
2019-07-23 15:38:07 -0400

    navbar finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import vehicles from "./assets/vehicles.json";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Search from "./components/Search";
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    vehicles,
+    searchResults: []
+  };
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Header />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          {this.state.searchResults.length === 0 ? <Header /> : <Search />}
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,63 @@
 import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar">
-        <div className="grid-container">
-          <div className="logo">
-            <img src={logo} alt="Car Rental Gateway" />
+      <Router>
+        <div className="navbar">
+          <div className="grid-container">
+            <div className="logo">
+              <img src={logo} alt="Car Rental Gateway" />
+            </div>
+            <nav>
+              <ul className="main-menu">
+                <li>
+                  <Link to="#">
+                    <a>Menu 1</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <a>
+                      Menu 2 <span>▼</span>
+                    </a>
+                  </Link>{" "}
+                  <ul className="sub-menu">
+                    <li>
+                      <Link to="#">
+                        <a>
+                          <span>Sub-menu</span>
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#">
+                        <a>
+                          <span>Sub-menu</span>
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#">
+                        <a>
+                          <span>Sub-menu</span>
+                        </a>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">
+                    <a>Hover</a>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-          <nav>
-            <ul>
-              <li>Menu 1</li>
-              <li>Menu 2</li>
-              <li>Menu 3</li>
-            </ul>
-          </nav>
         </div>
-      </div>
+      </Router>
     );
   }
 }
