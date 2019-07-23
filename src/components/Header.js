34979c88546dc25ca88a6651bf09bd79e5953c28
Corey <iam@coreylanier.com>
2019-07-23 19:37:57 -0400

    form formatting completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,88 @@
 import React from "react";
 
 class Header extends React.Component {
+  state = {
+    airport: "",
+    name: "",
+    email: "",
+    phone: "",
+    loading: false
+  };
+
+  changeState = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleForm = async e => {
+    e.preventDefault();
+    console.log("form submitted");
+    await this.setState({
+      loading: true
+    });
+  };
+
   render() {
     return (
       <header>
-        <div className="container">
-          <h1>Good deal!</h1>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
-          <p>scroll down</p>
+        <div className="container grid-container">
+          <div className="searchbox">
+            <form onSubmit={this.handleForm}>
+              <fieldset aria-busy={this.state.loading}>
+                {this.state.loading && <p className="loading">Loading...</p>}
+                <label htmlFor="airport">
+                  <p>Pick-up location:</p>
+                  <input
+                    type="text"
+                    name="airport"
+                    placeholder="Placeholder"
+                    onChange={this.changeState}
+                    required
+                    value={this.state.airport}
+                  />
+                </label>
+                <label htmlFor="name">
+                  <p>Name:</p>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Placeholder"
+                    onChange={this.changeState}
+                    required
+                    value={this.state.name}
+                  />
+                </label>
+                <label htmlFor="email">
+                  <p>E-mail:</p>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Placeholder"
+                    onChange={this.changeState}
+                    required
+                    value={this.state.email}
+                  />
+                </label>
+                <label htmlFor="phone">
+                  <p>Phone:</p>
+                  <input
+                    type="text"
+                    name="phone"
+                    placeholder="Placeholder"
+                    onChange={this.changeState}
+                    required
+                    value={this.state.phone}
+                  />
+                </label>
+                <input type="submit" value="Search" />
+              </fieldset>
+            </form>
+          </div>
+          <div className="title">
+            <h1>Best deals!</h1>
+          </div>
         </div>
       </header>
     );
